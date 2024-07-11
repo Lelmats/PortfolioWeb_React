2b53c35120b1f2647fc7518008795a9c1530bf33
Max Rivera <69810374+Lelmats@users.noreply.github.com>
2024-07-11 16:12:16 -0700

    UpdateNavbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,16 @@
+import {
+  Collapse,
+  Dropdown,
+  initTWE,
+} from "tw-elements";
+
 export const Navbar = () => {
+  
+initTWE({ Collapse, Dropdown });
+
   return (
     <nav
-      className="flex-no-wrap relative flex w-full items-center justify-between bg-zinc-50 py-2 shadow-dark-mild dark:bg-neutral-700 lg:flex-wrap lg:justify-start lg:py-4">
+      className="flex-no-wrap fixed sm:relative top-0 flex w-full items-center justify-between bg-[#FBFBFB] py-2 shadow-md shadow-black/5 dark:bg-neutral-600 dark:shadow-black/10 lg:flex-wrap lg:justify-start lg:py-4">
       <div className="flex w-full flex-wrap items-center justify-between px-3">
         <button
           className="block border-0 bg-transparent px-2 text-black/50 hover:no-underline hover:shadow-none focus:no-underline focus:shadow-none focus:outline-none focus:ring-0 dark:text-neutral-200 lg:hidden"
@@ -14,7 +23,7 @@ export const Navbar = () => {
           <span
             className="[&>svg]:w-7 [&>svg]:stroke-black/50 dark:[&>svg]:stroke-neutral-200">
             <svg
-              xmlns="http://www.w3.org/2000/svg"
+              // xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
               fill="currentColor">
               <path
@@ -32,21 +41,21 @@ export const Navbar = () => {
           <a
             className="mb-4 me-5 ms-2 mt-3 flex items-center text-neutral-900 hover:text-neutral-900 focus:text-neutral-900 dark:text-neutral-200 dark:hover:text-neutral-400 dark:focus:text-neutral-400 lg:mb-0 lg:mt-0"
             href="#">
-            <img
-              src="https://tecdn.b-cdn.net/img/logo/te-transparent-noshadows.webp"
+            {/* <img
+              // src="https://tecdn.b-cdn.net/img/logo/te-transparent-noshadows.webp"
               style={{height: '15px'}}
-              alt="TE Logo"
-              loading="lazy" />
+              // alt="TE Logo"
+              loading="lazy" /> */}
           </a>
           <ul
-            className="list-style-none me-auto flex flex-col ps-0 lg:flex-row"
+            className="list-style-none me-auto flex flex-col ps-0 lg:flex-row font-JetBrains"
             data-twe-navbar-nav-ref>
             <li className="mb-4 lg:mb-0 lg:pe-2" data-twe-nav-item-ref>
               <a
                 className="text-black/60 transition duration-200 hover:text-black/80 hover:ease-in-out focus:text-black/80 active:text-black/80 motion-reduce:transition-none dark:text-white/60 dark:hover:text-white/80 dark:focus:text-white/80 dark:active:text-white/80 lg:px-2"
                 href="#"
                 data-twe-nav-link-ref
-                >Dashboard</a
+                >Inicio</a
               >
             </li>
             <li className="mb-4 lg:mb-0 lg:pe-2" data-twe-nav-item-ref>
@@ -54,7 +63,15 @@ export const Navbar = () => {
                 className="text-black/60 transition duration-200 hover:text-black/80 hover:ease-in-out focus:text-black/80 active:text-black/80 motion-reduce:transition-none dark:text-white/60 dark:hover:text-white/80 dark:focus:text-white/80 dark:active:text-white/80 lg:px-2"
                 href="#"
                 data-twe-nav-link-ref
-                >Team</a
+                >AboutMe</a
+              >
+            </li>
+            <li className="mb-4 lg:mb-0 lg:pe-2" data-twe-nav-item-ref>
+              <a
+                className="text-black/60 transition duration-200 hover:text-black/80 hover:ease-in-out focus:text-black/80 active:text-black/80 motion-reduce:transition-none dark:text-white/60 dark:hover:text-white/80 dark:focus:text-white/80 dark:active:text-white/80 lg:px-2"
+                href="#"
+                data-twe-nav-link-ref
+                >Experience</a
               >
             </li>
             <li className="mb-4 lg:mb-0 lg:pe-2" data-twe-nav-item-ref>
@@ -67,9 +84,23 @@ export const Navbar = () => {
             </li>
           </ul>
         </div>
-    
+              
+        {/* RightBar */}
         <div className="relative flex items-center">
-          <a className="me-4 text-neutral-600 dark:text-white" href="#">
+        <ul
+            className="list-style-none me-auto flex flex-col ps-0 lg:flex-row font-JetBrains"
+            data-twe-navbar-nav-ref>
+            <li className="mb-4 lg:mb-0 lg:pe-2" data-twe-nav-item-ref>
+              <a
+                className="text-black/60 transition duration-200 hover:text-black/80 hover:ease-in-out focus:text-black/80 active:text-black/80 motion-reduce:transition-none dark:text-white/60 dark:hover:text-white/80 dark:focus:text-white/80 dark:active:text-white/80 lg:px-2"
+                href="#"
+                data-twe-nav-link-ref
+                >Dashboard</a
+              >
+            </li>
+          </ul>
+          {/* Cart  */}
+          {/* <a className="me-4 text-neutral-600 dark:text-white" href="#">
             <span className="[&>svg]:w-5">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -79,9 +110,9 @@ export const Navbar = () => {
                   d="M2.25 2.25a.75.75 0 000 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 00-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 000-1.5H5.378A2.25 2.25 0 017.5 15h11.218a.75.75 0 00.674-.421 60.358 60.358 0 002.96-7.228.75.75 0 00-.525-.965A60.864 60.864 0 005.68 4.509l-.232-.867A1.875 1.875 0 003.636 2.25H2.25zM3.75 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zM16.5 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0z" />
               </svg>
             </span>
-          </a>
-    
-          <div
+          </a> */}
+          {/* Notifications  */}
+          {/* <div
             className="relative"
             data-twe-dropdown-ref
             data-twe-dropdown-alignment="end">
@@ -137,9 +168,9 @@ export const Navbar = () => {
                 >
               </li>
             </ul>
-          </div>
-    
-          <div
+          </div> */}
+          {/* PersonIcon */}
+          {/* <div
             className="relative"
             data-twe-dropdown-ref
             data-twe-dropdown-alignment="end">
@@ -186,7 +217,7 @@ export const Navbar = () => {
                 >
               </li>
             </ul>
-          </div>
+          </div> */}
         </div>
       </div>
     </nav>
