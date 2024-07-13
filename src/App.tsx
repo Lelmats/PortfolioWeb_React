29ad24c596ef1031f648349be98e06ae8157729b
Max Rivera <69810374+Lelmats@users.noreply.github.com>
2024-07-12 18:12:28 -0700

    Technologies UpdateV2

--- src/App.tsx
+++ src/App.tsx
@@ -41,14 +41,37 @@ function App() {
     </div>
     {/* <br /> */}
     {/* Section 2 */}
-    <div className="bg-cover bg-backgroundPrimary flex p-28 sm:text-5xl md:text-5xl xl:text-6xl text-4xl " style={backgroundStyle} id='AboutMe' >
-      <div className='mx-auto'>
-        <h1 className='font-JetBrains border-b-8 border-secondaryColor text-white text-center'>AboutMe</h1> 
+    <div className="bg-cover flex bg-backgroundPrimary p-10 xl:p-28" style={backgroundStyle} id='AboutMe'>
+      <div className='md:grid md:grid-flow-row'>
+        <div className='mb-8 md:grid md:grid-flow-col'>
+          <h1 className='font-JetBrains mr-10 mb-4 border-b-8 border-secondaryColor text-white text-4xl sm:text-5xl md:text-5xl xl:text-6xl w-fit h-fit'>
+            About Me
+          </h1>
+          <p className='text-xl sm:text-xl md:text-2xl xl:text-3xl text-white 2xl:w-3/4 ml-auto text-justify'>
+            I'm a Software Developer with a strong background
+            in Front-End development and some experience in Back-End.
+            Currently, I'm deepening my knowledge in data structures
+            and constantly sharpening my skills.
+            I have a passion for learning new technologies,
+            and I am committed to continuous improvement.
+          </p>
+        </div>
+        <div className='md:grid md:grid-flow-col'>
+          <div className='flex sm:text-4xl md:text-4xl xl:text-6xl text-3xl items-center justify-center'>
+            <div className='mr-10 sm:mb-5 '>
+              <h1 className='font-JetBrains text-white border-b-8 border-secondaryColor md:mb-6 ml-auto sm:text-5xl md:text-5xl xl:text-6xl'>Technologies</h1> 
+              {/* <h1 className='font-JetBrains text-white text-center w-fit ml-auto sm:text-5xl md:text-5xl xl:text-6xl '>Experience &</h1>  */}
+            </div>
+          </div>
+          <div className=''>
+              <img src="/img/Tech_Icons.png" alt="" className='h-[25rem] xl:h-[30rem] object-contain mx-auto'/>
+          </div>
+        </div>
       </div>
     </div>
 
-    {/* Section 3 */}
-    <div className="bg-cover flex max-lg:grid bg-backgroundPrimary px-10 md:px-24 2xl:px-36 sm:text-5xl md:text-5xl xl:text-6xl text-4xl justify-center md:justify-center lg:justify-between items-center" style={backgroundStyle} id='Experience' >
+    {/* Section 2.5 */}
+    {/* <div className="bg-cover flex max-lg:grid bg-backgroundPrimary px-10 md:px-24 2xl:px-36 sm:text-5xl md:text-5xl xl:text-6xl text-4xl justify-center md:justify-center lg:justify-between items-center" style={backgroundStyle} id='Experience' >
         <div className='mx-10'>
           <h1 className='font-JetBrains text-white text-center '>Experience &</h1> 
           <h1 className='font-JetBrains border-b-8 border-secondaryColor text-white text-center md:mb-6'>Technologies</h1> 
@@ -56,8 +79,14 @@ function App() {
         <div>
           <img src="/img/Tech_Icons.png" alt="" className='h-[30rem] lg:h-[45rem] object-contain '/>
         </div>
-    </div>
+    </div> 
 
+    {/* Section 3 */}
+    <div className="bg-cover bg-backgroundPrimary flex p-16 sm:text-5xl md:text-5xl xl:text-6xl text-4xl" style={backgroundStyle} id='Projects' >
+      <div className='mx-auto'>
+        <h1 className='font-JetBrains border-b-8 border-secondaryColor text-white text-center '>Experience</h1> 
+      </div>
+    </div>
     {/* Section 4 */}
     <div className="bg-cover bg-backgroundPrimary flex p-28 sm:text-5xl md:text-5xl xl:text-6xl text-4xl" style={backgroundStyle} id='Projects' >
       <div className='mx-auto'>
