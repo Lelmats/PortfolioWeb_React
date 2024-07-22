4a7537e3a6f85f4390612b289f76ea7469865b6d
Max Rivera <69810374+Lelmats@users.noreply.github.com>
2024-07-22 15:23:52 -0700

    SafariBugs_V4

--- src/App.tsx
+++ src/App.tsx
@@ -37,7 +37,7 @@ function App() {
         </div>
         <div className='flex justify-end'>
           {/* <img className='object-cover h-[35vh] md:h-[65vh] sm:h-[55vh] min-w-fit hover:scale-105 transition-transform duration-500 ' src="img\MaxCharacter.png" alt="" /> */}
-          <img className='object-cover w-[35vw] md:h-[65vh] sm:h-[55vh] min-w-fit hover:scale-105 transition-transform duration-500 ' src="img\MaxCharacter.png" alt="" />
+          <img className='object-cover w-[10vw] md:h-[65vh] sm:h-[55vh] min-w-fit hover:scale-105 transition-transform duration-500 ' src="img\MaxCharacter.png" alt="" />
         </div>
       </div>
     </div>
